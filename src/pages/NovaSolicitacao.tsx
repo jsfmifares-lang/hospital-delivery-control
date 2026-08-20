6a89fbe4d59f2b73fa520b0ea52eaaed0046b423
jsfmifares-lang <jsf.mifares@gmail.com>
2026-08-19 21:02:24 -0300

    Envia paciente e observacao no insert

--- src/pages/NovaSolicitacao.tsx
+++ src/pages/NovaSolicitacao.tsx
@@ -43,6 +43,8 @@ export function NovaSolicitacao() {
       await createEntrega.mutateAsync({
         hospital_id: selectedHospital.id,
         nome_hospital: selectedHospital.nome,
+        nome_paciente: nomePaciente || undefined,
+        observacao: observacao || undefined,
         created_by: user?.username || "Desconhecido",
       });
 
