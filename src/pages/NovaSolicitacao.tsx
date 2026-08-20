59dae43af1dd897cf76e5a190bd5d3e717a397d9
jsfmifares-lang <jsf.mifares@gmail.com>
2026-08-19 21:05:40 -0300

    Removido observacao do insert - banco nao tem a coluna

--- src/pages/NovaSolicitacao.tsx
+++ src/pages/NovaSolicitacao.tsx
@@ -44,7 +44,6 @@ export function NovaSolicitacao() {
         hospital_id: selectedHospital.id,
         nome_hospital: selectedHospital.nome,
         nome_paciente: nomePaciente || undefined,
-        observacao: observacao || undefined,
         created_by: user?.username || "Desconhecido",
       });
 
